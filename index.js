ea7caf224352d279a227f417607bf6d4de95d2a1
Blake Stoner <stonerblake@gmail.com>
2019-03-16 15:35:14 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -20,9 +20,36 @@ function currentLine(line) {
 return "The line is currently empty."
  }
   
- for (let i = 0, l = line.length; i < l; i++) {
-    whosInLine.push(`${i + 1}. ${line[i]}`)
+ for (let i = 0, l = takeANumber; i < l; i++) {
+    takeANumber.push(`${i + 1}. ${line[i]}`)
   }
   return `The line is currently: ${whosInLine.join(', ')}`
 
   }
+  
+  // 2 part: total current ticket number of tickets + current position in line
+  
+  
+
+  
+  
+  function johnLennonFacts(facts) {
+  const jlFacts = []
+
+  let i = 0
+  while (i < facts.length) {
+    jlFacts.push(`${facts[i]}!!!`)
+    i++
+  }
+  
+  
+  function positionInLine (line, number) {
+  const TotalTickets = []
+  
+  let i = 0
+  while (i < takeANumber) { 
+    takeANumber.push(`Your order will be ready soon! Your ticket number is ${TodaysTickets(i)} and your currently ${currentLine} in line.`) 
+  
+  line.push(name)
+  return `Welcome, ${name}. You are number ${line.length} in line.`
+  
